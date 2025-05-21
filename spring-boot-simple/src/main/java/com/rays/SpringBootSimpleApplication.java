d48bb57d17c9b72311cc57d058cc2b3e045d39eb
abeetpatel <abeetpatel@outlook.com>
2025-05-22 05:05:47 +0530

    '22-05-2025'

--- spring-boot-simple/src/main/java/com/rays/SpringBootSimpleApplication.java
+++ spring-boot-simple/src/main/java/com/rays/SpringBootSimpleApplication.java
@@ -15,4 +15,15 @@ public class SpringBootSimpleApplication {
 
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfig() {
+		WebMvcConfigurer w = new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+			}
+		};
+		return w;
+	}
+
 }
